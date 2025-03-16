0ecf74206b87a3dec2301075ff41d7d336d5b050
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2025-03-16 11:23:07 +0530

    add mongoose connection function modified

--- redis-practice/config/db.js
+++ redis-practice/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 async function connectDB() {
     try {
-        const connectionInstance = await mongoose.connect();
+        const connectionInstance = await mongoose.connect('mongodb://localhost:27017/redis-testing');
+        if (connectionInstance) {
+            console.log(connectionInstance);
+        }
     } catch (error) {
         console.log(error);
     }
