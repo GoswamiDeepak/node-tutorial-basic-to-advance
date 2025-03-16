7953187dde233535db4588ea1044d272edad189d
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2025-03-16 11:10:09 +0530

    add route

--- redis-practice/router/main.js
+++ redis-practice/router/main.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
-const router = express.Router()
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome ' });
+});
 
 export default router;
